592a67ea67f6e1707ec5d4e1f3fdf9d6bb65cf63
Nelba Barreto <barretonelba@gmail.com>
2024-07-27 16:29:24 -0400

    Agregar resumen arriba del gráfico

--- app.py
+++ app.py
@@ -6,18 +6,19 @@ import plotly.graph_objects as go
 import pandas as pd
 
 # call the ability to add external scripts
-external_scripts = [
-    {"src": "https://cdn.tailwindcss.com"}
-]
+external_scripts = [{"src": "https://cdn.tailwindcss.com"}]
 
 # Styles
 bg_color = "black"
 
-main_button = "relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none"
-main_button_span = "text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 hover:font-bold"
+main_button = "relative inline-flex items-center justify-center p-1 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 focus:ring-4 focus:outline-none"
+main_button_span = "text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0 hover:font-bold"
+tile = "relative inline-flex items-center justify-center py-1 px-10 mb-2 me-2 overflow-hidden text-lg font-bold text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300"
 
 # Initialize the app
-app = dash.Dash(__name__, external_scripts=external_scripts, suppress_callback_exceptions=True)
+app = dash.Dash(
+    __name__, external_scripts=external_scripts, suppress_callback_exceptions=True
+)
 data = pd.read_csv(
     "https://raw.githubusercontent.com/NelbaBarreto/programacion-ciencias-datos/main/data/dinosaurs_dataset.csv"
 )
@@ -27,9 +28,11 @@ data["length"] = data["length"].astype(float)
 
 data.loc[data["period"] == "USA", "period"] = "Late Cretaceous"
 
+
 def substr_till_second_space(s):
     parts = s.split(" ")
     return " ".join(parts[:2])
+
 
 data["full_period"] = data["period"]
 data["period"] = data["full_period"].apply(substr_till_second_space)
@@ -37,12 +40,17 @@ data["period"] = data["full_period"].apply(substr_till_second_space)
 unique_periods = list(data["period"].unique())
 unique_periods.insert(0, "Todos")
 periods = [{"label": period, "value": period} for period in unique_periods]
+total_count = len(data)
+total_country_count = len(data.groupby("lived_in"))
 
 # Main layout
 app.layout = html.Div(
     className="container",
     children=[
-        html.H1("Análisis de Datos de Dinosaurios 🦕", className="text-5xl text-white mt-4 mb-6 text-center"),
+        html.H1(
+            "Análisis de Datos de Dinosaurios 🦕",
+            className="text-5xl text-white mt-4 mb-6 text-center",
+        ),
         html.Div(
             [
                 html.Button(
@@ -58,29 +66,73 @@ app.layout = html.Div(
                     children=html.Span("Periodo", className=main_button_span),
                 ),
             ],
-            className="flex justify-center"
+            className="flex justify-center",
         ),
         html.Div(id="page-content", className="p-20"),
     ],
 )
 
+
 # Layouts for different pages
 def layout_overview():
-    return html.Div([
-        dcc.Graph(id="grafico-dinosaurios", figure=dino_count_by_diet())
-    ])
+    return html.Div(
+        [
+            html.Div(
+                [
+                    html.Div(
+                        [
+                            html.Img(
+                                src=app.get_asset_url("icons8-dino-67.png"),
+                                className="mx-auto w-14 h-14 mb-2",
+                            ),
+                            html.Span(total_count),
+                            html.Br(),
+                            html.Span("Dinosaurios"),
+                        ],
+                        className="text-center",
+                    )
+                ],
+                className=tile,
+            ),
+            html.Div(
+                [
+                    html.Div(
+                        [
+                            html.Img(
+                                src=app.get_asset_url("icons8-earth-100.png"),
+                                className="mx-auto w-14 h-14 mb-2",
+                            ),
+                            html.Span(total_country_count),
+                            html.Br(),
+                            html.Span("Países"),
+                        ],
+                        className="text-center",
+                    )
+                ],
+                className=tile,
+            ),
+            dcc.Graph(id="grafico-dinosaurios", figure=dino_count_by_diet()),
+        ]
+    )
+
 
 def layout_periodo():
-    return html.Div([
-        html.H2("Elegir un Periodo:", className="text-lime-500 font-semibold text-sm mb-2"),
-        dcc.Dropdown(
-            id="dropdown-period",
-            options=periods,
-            value="Todos",
-            className="w-1/2",
-        ),
-        dcc.Graph(id="grafico-dinosaurios"),
-    ])
+    return html.Div(
+        [
+            html.H2(
+                "Elegir un Periodo:",
+                className="text-lime-500 font-semibold text-sm mb-2",
+            ),
+            dcc.Dropdown(
+                id="dropdown-period",
+                options=periods,
+                value="Todos",
+                className="w-1/2",
+            ),
+            dcc.Graph(id="grafico-dinosaurios"),
+        ]
+    )
+
 
 # Graph for dinosaur count by diet
 def dino_count_by_diet():
@@ -98,10 +150,15 @@ def dino_count_by_diet():
     )
     return fig
 
+
 # Graph for dinosaur count by period
 def dino_count_by_period(filtered_data):
     fig = go.Figure(
-        data=[go.Histogram(x=filtered_data["period"], texttemplate="%{y}", textfont_size=15)],
+        data=[
+            go.Histogram(
+                x=filtered_data["period"], texttemplate="%{y}", textfont_size=15
+            )
+        ],
         layout=dict(
             barcornerradius=15,
         ),
@@ -116,27 +173,29 @@ def dino_count_by_period(filtered_data):
     )
     return fig
 
+
 # Callback to handle button clicks and update the page content
 @app.callback(
     Output("page-content", "children"),
-    [Input("btn-overview", "n_clicks"), Input("btn-periodo", "n_clicks")]
+    [Input("btn-overview", "n_clicks"), Input("btn-periodo", "n_clicks")],
 )
 def display_page(n_clicks_overview, n_clicks_periodo):
     ctx = dash.callback_context
     if not ctx.triggered:
         return layout_overview()
     else:
-        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-        if button_id == 'btn-overview':
+        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+        if button_id == "btn-overview":
             return layout_overview()
-        elif button_id == 'btn-periodo':
+        elif button_id == "btn-periodo":
             return layout_periodo()
+
 
 # Callback to update the graph based on dropdown selection
 @app.callback(
     Output("grafico-dinosaurios", "figure"),
     [Input("dropdown-period", "value")],
-    [State("btn-periodo", "n_clicks")]
+    [State("btn-periodo", "n_clicks")],
 )
 def update_graph(selected_period, n_clicks_periodo):
     if n_clicks_periodo is None:
@@ -146,8 +205,9 @@ def update_graph(selected_period, n_clicks_periodo):
         filtered_data = data
     else:
         filtered_data = data[data["period"] == selected_period]
-    
+
     return dino_count_by_period(filtered_data)
+
 
 # Run the app
 if __name__ == "__main__":
